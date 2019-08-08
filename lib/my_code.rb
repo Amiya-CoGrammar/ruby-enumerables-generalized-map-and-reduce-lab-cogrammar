1d2e1f0c9c6ec6095fc7609357e192c6609f3f10
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 15:39:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,19 @@ def map(x)
 
 end
 map([1, 2, 3, -9]){|n| n * -1}
+
+def reduce(x, starting_point = nil)
+  if starting_point
+    i = 0
+    var_x = starting_point
+  else
+    i = 1
+    var_x = x[0]
+  end
+
+  while i < x.length do
+    var_x = (yield(var_x, x[i]))
+    i += 1
+  end
+  return var_x
+  end
