4409c17625844113f74e54d74d7ac1ba8f170d4a
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 15:35:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,11 +2,10 @@ def map(x, element)
   new = []
 
   while i < x.length do
-    new.push( yield(source_array[i]))
+    new.push( yield(x[i]))
     i += 1
   end
   return new
-
 
 end
 map([1, 2, 3, -9]){|n| n * -1})
